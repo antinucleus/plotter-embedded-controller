a0efacdf9baac3df36f6b7f75eade9d029c7c00f
antinucleus <antinucleuss@gmail.com>
2024-05-21 22:27:25 +0300

    fix(main): comment out main code

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,15 @@ const int SELECTED_MODE = SIXTEENTH_STEP;
 
 int main()
 {
+    wiringPiSetup();
+    initPins();
+    disableOutput();
+    setDriverStatus('x', DRIVER_ENABLE);
+    setDriverStatus('y', DRIVER_ENABLE);
+    softServoSetup(23, 23, 23, 23, 23, 23, 23, 23);
+
+    double oneStepDistanceX = getOneStepDistance(SELECTED_MODE);
+    double oneStepDistanceY = getOneStepDistance(SELECTED_MODE);
     string read;
     string part;
     ifstream ReadFile("test.gcode");
@@ -28,118 +37,69 @@ int main()
     short isStarted = 0;
     string response;
 
-    while (getline(ReadFile, read))
+    cout << "Auto Homing...\n";
+    moveTool('+');
+    autoHome();
+    setDriveMode('x', SELECTED_MODE);
+    setDriveMode('y', SELECTED_MODE);
+    isStarted = 1;
+
+    for (;;)
     {
-        stringstream s(read);
-        // printf("currentX: %f  currentY: %f\n", currentX, currentY);
-
-        while (s >> part)
+        if (isStarted == 1)
         {
-            if (part[0] == 'X')
+
+            while (getline(ReadFile, read))
             {
-                pointX = stod(part.substr(1));
-                targetX = abs(pointX - currentX);
-                directionX = pointX < currentX ? '-' : '+';
+                stringstream s(read);
+                // printf("currentX: %f  currentY: %f\n", currentX, currentY);
+
+                while (s >> part)
+                {
+                    if (part == "G0")
+                    {
+                        moveTool('+');
+                    }
+
+                    if (part == "G1")
+                    {
+                        moveTool('-');
+                    }
+
+                    if (part[0] == 'X')
+                    {
+                        pointX = stod(part.substr(1));
+                        targetX = abs(pointX - currentX);
+                        directionX = pointX < currentX ? '-' : '+';
+                    }
+
+                    if (part[0] == 'Y')
+                    {
+                        pointY = stod(part.substr(1));
+                        targetY = abs(pointY - currentY);
+                        directionY = pointY < currentY ? '+' : '-'; // CHECK HERE FOR COORDINATE ROTATION
+                    }
+                }
+
+                cout << "dirX: " << directionX << "dirY: " << directionY << "tarX: " << targetX << "tarY: " << targetY << endl;
+                cout << "Response: " << postData(currentX, currentY) << endl;
+
+                moveAxis(directionX, targetX, directionY, targetY, oneStepDistanceX, oneStepDistanceY);
+
+                currentX = pointX;
+                currentY = pointY;
             }
 
-            if (part[0] == 'Y')
-            {
-                pointY = stod(part.substr(1));
-                targetY = abs(pointY - currentY);
-                directionY = pointY < currentY ? '+' : '-'; // CHECK HERE FOR COORDINATE ROTATION
-            }
+            printf("End of program :)\n");
+            moveTool('+');
+            serveBed();
+            moveTool('-');
+            setDriverStatus('x', DRIVER_DISABLE);
+            setDriverStatus('y', DRIVER_DISABLE);
+            isStarted = 0;
+            break;
         }
-        cout << "dirX: " << directionX << "dirY: " << directionY << "tarX: " << targetX << "tarY: " << targetY << endl;
-
-        currentX = pointX;
-        currentY = pointY;
-
-        cout << "Response: " << postData(currentX, currentY) << endl;
     }
-
-    // wiringPiSetup();
-    // initPins();
-    // disableOutput();
-    // setDriverStatus('x', DRIVER_ENABLE);
-    // setDriverStatus('y', DRIVER_ENABLE);
-    // softServoSetup(23, 23, 23, 23, 23, 23, 23, 23);
-
-    // double oneStepDistanceX = getOneStepDistance(SELECTED_MODE);
-    // double oneStepDistanceY = getOneStepDistance(SELECTED_MODE);
-    // string read;
-    // string part;
-    // ifstream ReadFile("test.gcode");
-    // double currentX = 0.0f;
-    // double currentY = 0.0f;
-    // double pointX = 0.0f;
-    // double pointY = 0.0f;
-    // double targetX = 0.0f;
-    // double targetY = 0.0f;
-    // char directionX, directionY;
-    // short isStarted = 0;
-
-    // cout << "Auto Homing...\n";
-    // moveTool('+');
-    // autoHome();
-    // setDriveMode('x', SELECTED_MODE);
-    // setDriveMode('y', SELECTED_MODE);
-    // isStarted = 1;
-
-    // for (;;)
-    // {
-    //     if (isStarted == 1)
-    //     {
-
-    //         while (getline(ReadFile, read))
-    //         {
-    //             stringstream s(read);
-    //             // printf("currentX: %f  currentY: %f\n", currentX, currentY);
-
-    //             while (s >> part)
-    //             {
-    //                 if (part == "G0")
-    //                 {
-    //                     moveTool('+');
-    //                 }
-
-    //                 if (part == "G1")
-    //                 {
-    //                     moveTool('-');
-    //                 }
-
-    //                 if (part[0] == 'X')
-    //                 {
-    //                     pointX = stod(part.substr(1));
-    //                     targetX = abs(pointX - currentX);
-    //                     directionX = pointX < currentX ? '-' : '+';
-    //                 }
-
-    //                 if (part[0] == 'Y')
-    //                 {
-    //                     pointY = stod(part.substr(1));
-    //                     targetY = abs(pointY - currentY);
-    //                     directionY = pointY < currentY ? '+' : '-'; // CHECK HERE FOR COORDINATE ROTATION
-    //                 }
-    //             }
-
-    //             cout << "dirX: " << directionX << "dirY: " << directionY << "tarX: " << targetX << "tarY: " << targetY << endl;
-
-    //             moveAxis(directionX, targetX, directionY, targetY, oneStepDistanceX, oneStepDistanceY);
-
-    //             currentX = pointX;
-    //             currentY = pointY;
-    //         }
-
-    //         printf("End of program :)\n");
-    //         moveTool('+');
-    //         serveBed();
-    //         moveTool('-');
-    //         setDriverStatus('x', DRIVER_DISABLE);
-    //         setDriverStatus('y', DRIVER_DISABLE);
-    //         isStarted = 0;
-    //         break;
-    //     }
-    // }
 
     return 0;
 }
